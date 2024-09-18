17fb7a62636d2a4a1c64eee4ebfc22e5a0832034
unknown <ma475671@uaeh.edu.mx>
2024-09-18 15:25:22 -0600

    events section

---
+++ src/components/Eventos.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Eventos = () => {
+  return (
+    <section className='flex flex-col mt-12 mb-32'>
+        <div className='flex flex-col items-center lg:items-start lg:ml-60'>
+            <h1 className='text-7xl lg:text-9xl text-[#0371a4] font-bold font-bebas'>
+                EVENTOS
+            </h1>
+
+            <p className="text-sm lg:text-xl text-center font-jetbrains mt-4 max-w-6xl whitespace-normal break-words px-6 lg:px-0 text-black">
+                Nos esforzamos por ofrecer una amplia gama de actividades diseñadas para enriquecer la experiencia educativa de nuestros miembros, así como para fomentar el crecimiento profesional y personal.
+            </p>
+        </div>
+    </section>
+  )
+}
